de1b06de73a738ffc49bced104111334c7c1808f
saihruday1997 <saihruday.08@gmail.com>
2020-12-17 23:12:31 +0530

    chngs

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const app = express()
 const bodyParser = require("body-parser");
 const port = 3000
 app.use(express.urlencoded());
+let posts = require("./initialData");
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
@@ -12,6 +13,16 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
 // your code goes here
+
+app.get("/posts", (req, res) => {
+    let arr = posts.slice(0, 10);
+    res.status(200).send(arr);
+});
+
+app.get("/posts?max=15", (req, res) => {
+    let arr = posts.slice(0, 15);
+    res.status(200).send(arr);
+});
 
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
